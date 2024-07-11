37c96351d36f376f96f9dda82cd00724c98a04c1
Sebastian Nickel <nick@nine.ch>
2024-07-11 11:53:22 +0200

    always fail

--- main.go
+++ main.go
@@ -1,61 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
-)
-
-var (
-	appVersion = "trunc"
-)
-
-const (
-	defaultPort     = 8080
-	portEnvVariable = "PORT"
+	"time"
 )
 
 func main() {
-	port := selectPort()
-	http.HandleFunc("/", showIP)
-	http.HandleFunc("/version", showVersion)
-	log.Printf("starting server on %s\n", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func showVersion(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, fmt.Sprintf("app version: %s", appVersion))
-}
-
-func showIP(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://ifconfig.me")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		io.WriteString(w, fmt.Sprintf("error on gathering public IP: %v", err))
-		return
-	}
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, fmt.Sprintf("public IP: %s", string(body)))
-}
-
-func selectPort() string {
-	defPort := fmt.Sprintf(":%d", defaultPort)
-	if port := os.Getenv(portEnvVariable); port != "" {
-		portNumber, err := strconv.Atoi(port)
-		if err != nil {
-			log.Printf("can not parse %s as a number: %v\n", port, err)
-			return defPort
-
-		}
-		return fmt.Sprintf(":%d", portNumber)
-	}
-	return defPort
+	log.Printf("failing server in 5 seconds...")
+	time.Sleep(5 * time.Second)
+	log.Fatal("failed...")
 }
